7941480c2120e1f075f51d3c11b074278ba0b53c
Hector-Santos <habs.404@gmail.com>
2022-07-07 19:53:26 -0300

    completado front-end login

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import GlobalStyle from "../styles/global";
 import Carrinho from "./carrinho";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TokenProvider from "../context/TokenContext";
+import Login from "./Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <TokenProvider>
         <Routes>
           <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </TokenProvider>
     </BrowserRouter>
