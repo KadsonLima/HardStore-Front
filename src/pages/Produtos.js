d643ff0aace502475f407ab09ad9f37a4a91a009
Hector-Santos <habs.404@gmail.com>
2022-07-10 19:45:34 -0300

    implementada tela de produtos - rotas mudadas para localhost

--- src/pages/Produtos.js
+++ src/pages/Produtos.js
@@ -5,19 +5,54 @@ import styled from "styled-components";
 import imglogo from "../assets/imglogo.png"
 import { TokenContext } from '../context/TokenContext';
 
+ function Produto({item}){
+    const [selected, setSelected] = useState(false)
+    const {token, header} = useContext(TokenContext)
+  function adicionarAoCarrinho(id){
+    let body={
+        id: id
+        }
+    if(selected){
+    setSelected(false)
+    axios.putg("http://localhost:5000/cart", body, header)
+          .then(response =>{
+            console.log(response)
+        })
 
+    }else{
+     setSelected(true)
+    
+     axios.post("http://localhost:5000/cart", body, header)
+          .then(response =>{
+      console.log(response)
+  })
+     }
+    }
+    return(
+        <Item selected= {selected}>
+        <div>
+            <img src={item.imagem} alt={item.nome}/>
+              <h2>R$ {item.preco}</h2>
+            </div>
+             <div>
+            <div className="name-icon"> <h1> {item.nome}</h1> <ion-icon onClick={()=> adicionarAoCarrinho(item._id)} name="cart-outline"></ion-icon></div>
+              <div className="descricao"> {item.description}</div>
+            </div>
+        </Item>
+    )
+}
 export default function Produtos(){ 
   const [produtos, setProdutos] = useState();
-  const [valor, setValor] = useState();
   const {token, header} = useContext(TokenContext)
+
+  
 
   useEffect(()=>{
 
-    axios.get("https://hardstore0.herokuapp.com/produtos",header)
+    axios.get("http://localhost:5000/produtos",header)
         .then(response =>{
           console.log(response)
-          setProdutos(response.data.produtos);
-          setValor(response.data.valor);
+          setProdutos(response.data)
         })
   }, [])
 
@@ -37,19 +72,10 @@ export default function Produtos(){
     <Container>
      {produtos?.map(((item, index)=>{
       return ( 
-        <Produto key={index}>
-          <div>
-            <img src={item.imagem} alt={item.nome}/>
-              <h1>R$ {item.preco}</h1>
-            </div>
-             <div>
-            <div className="name-icon"> <h1>R$ {item.nome}</h1> <ion-icon name="cart-outline"></ion-icon></div>
-              <div className="descricao">R$ {item.descricao}</div>
-            </div>
-            
-        
+        <Produto key={index} item = {item}>
         </Produto>)
         }))}
+
     </Container>
     </React.Fragment>
   );
@@ -57,7 +83,7 @@ export default function Produtos(){
 
 const Container = styled.div`
 background-color: #FFFFFF;
-height: 80vh;
+height: 100%;
 
 `
 const Header = styled.div`
@@ -90,6 +116,52 @@ width: 300px;
 }
 }
 `
-const Produto = styled.div`
-display: flex;
+
+
+const Item = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  min-height: 18vh;
+  width: 100vw;
+  background-color: #ededed;
+  padding: 10px;
+  justify-content: flex-start;
+  div{
+   margin-right: 10px;
+  h1{
+    font-size: 20px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 600;
+    color:black;
+    }
+ h2{
+    font-size: 18px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    }
+  img{
+    width: 80px;
+    height: 80px;
+  }
+  .name-icon{
+    display: flex;
+    justify-content: space-between;
+    font-size: 30px;
+  }
+  .descricao{
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    line-break: normal;
+    background-color: white;
+    min-height: 8vh;
+    height: auto;
+    width: 70vw;
+    margin-top: 10px;
+    margin-top: 5%;
+  }
+  .name-icon{
+    color:${props => props.selected? "#2193ff": "black" };
+  } 
+  }
 `
